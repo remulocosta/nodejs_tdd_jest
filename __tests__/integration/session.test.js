aece96593db57e8ecd56e6d043a4aba7a78a3ec9
Remulo Costa <remulo.costa@gmail.com>
2019-12-30 15:14:00 -0300

    created middleware and config jwt token

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -54,5 +54,31 @@ describe('Autentication', () => {
     expect(response.body).toHaveProperty('token');
   });
 
+  it('should be able to access private routes when autenticated', async () => {
+    const user = await factory.create('User', {
+      password: '123123'
+    });
+
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should not be able to access private routes without jwt token', async () => {
+    const response = await request(app).get('/dashboard');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with invalid jwt token ', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer 123123`);
+
+    expect(response.status).toBe(401);
+  });
+
 
 });
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 const routes = require('express').Router();
 
+const authMiddleware = require('./app/middleware/auth');
+
 const SessionController = require('./app/controllers/SessionController');
 
  // Definições de Rotas
  routes.post('/sessions', SessionController.strore);
+
+ routes.use(authMiddleware);
+
+ routes.get('/dashboard', (req, res) => {
+   return res.status(200).send();
+ })
 
 
 module.exports = routes;
